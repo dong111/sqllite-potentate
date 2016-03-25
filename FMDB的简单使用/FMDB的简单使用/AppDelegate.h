a3b2fcafb93501fd8cc33a56b3f5d1c4e7562c75
陈栋 <chendong@yitaosoft.com>
2016-03-25 13:08:36 +0800

    非代码 fmdb的简单使用
    非代码 fmdb的简单使用

---
+++ FMDB的简单使用/FMDB的简单使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDB的简单使用
+//
+//  Created by 陈栋 on 16/3/25.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FMDB的简单使用/FMDB的简单使用/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  FMDB的简单使用
+//
+//  Created by 陈栋 on 16/3/25.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+
+
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+  
+    
+    
+    
+}
+
+
+
+@end
